9d317443a413292a98c6289cce38dc7354027189
Diwakar <the.walkersingh@hotmail.com>
2025-04-26 07:11:06 +0530

    CRUD started

---
+++ cinemate-backend/src/main/java/com/thewa/cinematebackend/CinemateBackendApplication.java
@@ -0,0 +1,10 @@
+package com.thewa.cinematebackend;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemateBackendApplication {
+   public static void main(String[] args) {
+	  SpringApplication.run(CinemateBackendApplication.class, args);
+   }
+}
---
+++ cinemate-backend/src/test/java/com/thewa/cinematebackend/CinemateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thewa.cinematebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
